4d8c4e0a03dc549cb7eace853990c1344bedf3c4
WolffLoney <luccafelipe99@gmail.com>
2026-06-13 12:42:11 -0300

    feat: adicionar rotas do CRUD de livros

--- index.php
+++ index.php
@@ -11,6 +11,26 @@ switch ($acao) {
         $controller->listar();
         break;
 
+    case "cadastrar":
+        $controller->cadastrar();
+        break;
+
+    case "salvar":
+        $controller->salvar();
+        break;
+
+    case "editar":
+        $controller->editar();
+        break;
+
+    case "atualizar":
+        $controller->atualizar();
+        break;
+
+    case "excluir":
+        $controller->excluir();
+        break;
+
     default:
         $controller->listar();
         break;
